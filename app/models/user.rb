e9281a2ee8276cee6c30a3a33081ac4268e8f59e
yasuda <tasssra@gmail.com>
2019-04-24 15:58:11 +0900

    例外処理、インポートの成否メッセージを追加。完成

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,19 @@ class User < ApplicationRecord
 
   # インポート処理
   def self.import(path)
-
+    list = []
     # headers: true によって一行目にどんなデータがあるかのヘッダーがあることを教える
     CSV.foreach(path, headers: true) do |row|
-      User.create(
-              name: row["name"],
-              age: row["age"],
-              address: row["address"]
-      )
+      list << {
+        name: row["name"],
+        age: row["age"],
+        address: row["address"]
+      }
     end
+    puts "インポート開始"
+    User.create!(list) # !をつけることによりimportが失敗したらエラーが出る(つけないとimport失敗に気づけない)
+    puts "インポートに成功しました"
+  rescue ActiveModel::UnknownAttributeError => invalid # 上のself.importメソッドを対象に例外処理をする
+    puts "インポートに失敗しました: #{invalid}"
   end
 end
